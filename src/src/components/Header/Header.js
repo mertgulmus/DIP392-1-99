5ca5b9f2d8ba7868c29244749e14be65d8eb998f
Mert Gulmus <mert@gulmus.me>
2024-05-18 10:37:38 +0300

    installed sass and initialized Header and Homepage

---
+++ src/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.style.scss';
+
+function Header () {
+  return (
+    <header>
+      <h1>Connect4</h1>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/src/routes/Homepage/Homepage.js
@@ -0,0 +1,11 @@
+import './Homepage.style.scss';
+
+function Homepage() {
+  return (
+    <div class="Homepage">
+        <h1>Homepage</h1>
+    </div>
+  );
+}
+
+export default Homepage;
