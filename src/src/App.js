ec57c8161e2c6d70e097c964f4ae3c68d822ea95
Mert Gulmus <mert@gulmus.me>
2024-05-18 11:04:26 +0300

    added play component

--- src/src/App.js
+++ src/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Homepage from "./routes/Homepage";
 import NotFound from "./routes/NotFound";
+import Play from "./routes/Play";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<Homepage />} />
+                <Route path="/play" element={<Play />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
---
+++ src/src/routes/Play/Play.js
@@ -0,0 +1,14 @@
+import { PureComponent } from "react";
+import './Play.style.scss';
+
+export class Play extends PureComponent {
+    render() {
+        return (
+        <div class="Play">
+            <h1>Play</h1>
+        </div>
+        );
+    }
+}
+
+export default Play;
